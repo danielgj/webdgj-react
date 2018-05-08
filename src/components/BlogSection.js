baeac175b99789cf8955a451e03abfd4fc1562e8
Daniel García Jones <danielgj@gmail.com>
2018-05-08 12:14:27 +0200

    Iframe for blog and fix issues

--- src/components/BlogSection.js
+++ src/components/BlogSection.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const BlogSection  = () => (
 
-    <div  className="container">
-     <iframe style={{width: '98%', height: '100vh', position: 'relative'}} src="https://danielgarciajones.wordpress.com" frameborder="0" allowfullscreen></iframe>         
+    <div  className="container blogContain">
+     <iframe title="blog" style={{width: '98%', height: '100vh', position: 'relative'}} src="https://danielgarciajones.wordpress.com" frameborder="0" allowfullscreen></iframe>         
     </div>
 
 
